15c904cbd3915c52e56eb2e23536820da5d155d5
Alberto Navarro Pérez <alberto.navarro@altipal.com.co>
2025-08-20 22:54:03 -0500

    feat: actualizar handler para usar APIGatewayProxyEventV2 y retornar un objeto JSON

--- amplify/my-first-function-demo/handler.ts
+++ amplify/my-first-function-demo/handler.ts
@@ -1,3 +1,13 @@
-export const handler = async (event: unknown): Promise<string> => {
-  return `Hello from my first function! ${JSON.stringify(event)}`;
-};
+// amplify/my-first-function/handler.ts
+import type { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda'
+
+export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
+  return {
+    statusCode: 200,
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify({
+      message: 'Hello from my first function!',
+      event
+    })
+  }
+}
